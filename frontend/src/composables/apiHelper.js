29aa10ba18c3edf913852ef1fa9f73b086e91980
Fanny Gautier <fannygautier92@gmail.com>
2025-09-18 10:28:11 +0200

    fix: change api base_url

--- frontend/src/composables/apiHelper.js
+++ frontend/src/composables/apiHelper.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_API_BASE_URL || 'http://localhost:3000/api',
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL || 'http://localhost:3000/api',
 });
 
 export default apiClient;
